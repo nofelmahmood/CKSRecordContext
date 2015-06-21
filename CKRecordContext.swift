1c57143983c1f0942b12a42a90893704aed6d9db
nofelmahmood <nofelmehmood@gmail.com>
2015-06-21 19:17:23 +0500

    Documenting

--- CKRecordContext.swift
+++ CKRecordContext.swift
@@ -17,23 +17,55 @@ class CKRecordContext: NSObject {
     var modifiedRecords:Array<CKRecord> = Array<CKRecord>()
     var database:CKDatabase?
     
+    /**
+    Initializes the context with an instance of CKDatabase.
+    
+    :param: database   Database to use for performing operations in the context. Private or Public
+    
+    */
     init(database:CKDatabase?) {
         
         self.database = database
         super.init()
     }
+    
+    /**
+    Inserts and returns a new CKRecord with the specified record type in the context.
+    
+    :param: recordType   RecordType to use to initialize a new CKRecord
+    
+    :returns: An instance of newly initialized CKRecord
+    */
     func insertNewCKRecord(recordType:String)->CKRecord
     {
         var ckRecord:CKRecord = CKRecord(recordType: recordType)
         self.modifiedRecords.append(ckRecord)
         return ckRecord
     }
+    
+    /**
+    Inserts and returns a new CKRecord with the specified record type and recordID in the context.
+    
+    :param: recordType   RecordType to use to initialize a new CKRecord
+    :param: recordID     CKRecordID to use to initialize a new CKRecord
+    
+    :returns: An instance of newly initialized CKRecord
+    */
     func insertNewCKRecord(recordType:String,recordID:CKRecordID)->CKRecord
     {
         var ckRecord:CKRecord = CKRecord(recordType: recordType, recordID: recordID)
         self.modifiedRecords.append(ckRecord)
         return ckRecord
     }
+    
+    /**
+    Inserts and returns a new CKRecord with the specified record type and recordZoneID in the context.
+    
+    :param: recordType       RecordType to use to initialize a new CKRecord
+    :param: recordZoneID     CKRecordZoneID to use to initialize a new CKRecord
+    
+    :returns: An instance of newly initialized CKRecord
+    */
     func insertNewCKRecord(recordType:String,recordZoneID:CKRecordZoneID)->CKRecord
     {
         var ckRecord:CKRecord = CKRecord(recordType: recordType, zoneID: recordZoneID)
@@ -41,6 +73,13 @@ class CKRecordContext: NSObject {
         return ckRecord
     }
     
+    /**
+    Fetches and returns an instance of CKRecord with the provided recordID.
+    
+    :param: recordID   CKRecordID to use to fetch the record
+    
+    :returns: An instance of CKRecord
+    */
     func fetchCKRecord(recordID:CKRecordID)->CKRecord?
     {
         var fetchRecordsOperation:CKFetchRecordsOperation = CKFetchRecordsOperation(recordIDs: [recordID])
